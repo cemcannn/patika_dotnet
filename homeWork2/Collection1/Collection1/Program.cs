bb6439b14dacb82a52e19280ff258c90543149ef
cemcannn <cmcan@windowslive.com>
2022-06-29 17:05:21 +0300

    Instead of Write each of ArrayList, Average of every ArrayList and Count of every ArrayList added.

--- homeWork2/Collection1/Collection1/Program.cs
+++ homeWork2/Collection1/Collection1/Program.cs
@@ -7,52 +7,50 @@ namespace Collection1
     {
         static void Main(string[] args)
         {
-            ArrayList primeNumbers = new ArrayList();
-            ArrayList notPrimeNumbers = new ArrayList();
+            ArrayList primeNumbers = new ArrayList(); // Asal sayılar ArrayList olarak oluşturuldu.
+            ArrayList notPrimeNumbers = new ArrayList(); //Asal olmayan sayılar ArrayList olarak oluşturuldu.
+            int sumPrime = 0;
+            int sumNotPrime = 0;
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++) //20 sayılık for döngüsüü
             {
-                int count = 0;
+                int count = 0; 
                 string value = Console.ReadLine();
                 int num;
-                bool isNumeric = int.TryParse(value, out num);
+                bool isNumeric = int.TryParse(value, out num); //Kullanıcıdan alınan value değerini TryParse metodu ile numeric olup olmadığını kontrol ediyoruz.
                 
-                if(isNumeric && num>0)
+                if(isNumeric && num>0) //Değer numeric ve 0'dan büyük ise sisteme alıyor.
                 {
-                    for (int j = 1; j < num; j++)
+                    for (int j = 1; j < num; j++) //1'den başlayıp num-1 sayısına kadar bölüyor.
                     {
-                        if (num % j == 0)
+                        if (num % j == 0) //Bölüm 0 ise yani tam bölünüyorsa, sayacı 1 arttırıyor.
                         {
                             count++;
                         }
                     }
-                    if (count > 1)
+                    if (count > 1) //Eğer asal sayılara ekleyeceksek sayacın 1'den büyük olması bekleniyor, çünkü 1 sayısına da tam bölünüyor. Eğer 1'den fazla ise asal olmayan sayı listesine ekleniyor.
                     {
                         notPrimeNumbers.Add(num);
+                        sumNotPrime += num;
                     }
                     else
                     {
                         primeNumbers.Add(num);
+                        sumPrime += num;
                     }
-                }
-                
-
+                }  
             }
-            primeNumbers.Sort();
+
+            primeNumbers.Sort();//Listeleri sıralama işlemi.
+            primeNumbers.Reverse(); //Sıralamayı tersine çevirme
             notPrimeNumbers.Sort();
+            notPrimeNumbers.Reverse();
 
-            Console.Write("Prime Numbers : ");
-            foreach (var item in primeNumbers)
-            {
-                Console.Write(item + ", ");
-            }
+            Console.WriteLine("Count of Prime Numbers : " + primeNumbers.Count);
+            Console.WriteLine("Summary of Prime Numbers : " + sumPrime/primeNumbers.Count);
 
-            Console.WriteLine();
-            Console.Write("Not Prime Numbers : ");
-            foreach (var item in notPrimeNumbers)
-            {
-                Console.Write(item + ", ");
-            }
+            Console.WriteLine("Count of Not Prime Numbers : " + notPrimeNumbers.Count);
+            Console.WriteLine("Summary of Not Prime Numbers : " + sumNotPrime/notPrimeNumbers.Count);
         }
     }
 }
