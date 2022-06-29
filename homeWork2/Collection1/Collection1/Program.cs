dd7620073488632bb22d64e5ef388caec4df4dbe
cemcannn <cmcan@windowslive.com>
2022-06-29 14:49:49 +0300

    Collections Home Work 1 added.

---
+++ homeWork2/Collection1/Collection1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace Collection1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList primeNumbers = new ArrayList();
+            ArrayList notPrimeNumbers = new ArrayList();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int count = 0;
+                string value = Console.ReadLine();
+                int num;
+                bool isNumeric = int.TryParse(value, out num);
+                
+                if(isNumeric && num>0)
+                {
+                    for (int j = 1; j < num; j++)
+                    {
+                        if (num % j == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count > 1)
+                    {
+                        notPrimeNumbers.Add(num);
+                    }
+                    else
+                    {
+                        primeNumbers.Add(num);
+                    }
+                }
+                
+
+            }
+            primeNumbers.Sort();
+            notPrimeNumbers.Sort();
+
+            Console.Write("Prime Numbers : ");
+            foreach (var item in primeNumbers)
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Not Prime Numbers : ");
+            foreach (var item in notPrimeNumbers)
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
