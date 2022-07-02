570a095f0446cc2a5824d16fbcb2fe2598550f85
cemcannn <cmcan@windowslive.com>
2022-07-03 00:33:02 +0300

    Project destination changed, bool flag removed, README.md added.

---
+++ PhoneBookProject/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace PhoneBookProject
+{
+    class Contact
+    {
+        private string firstName;
+        private string lastName;
+        private string phoneNumber;
+
+        public string FirstName { get => firstName; set => firstName = value; } 
+        public string LastName { get => lastName; set => lastName = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+    }
+}
