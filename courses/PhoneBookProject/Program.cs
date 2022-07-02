ca3605313d522cadeb89486f7990bf7dc02fc75e
cemcannn <cmcan@windowslive.com>
2022-07-03 00:22:04 +0300

    PhoneBook project added. PhoneBook class and Contact class are created. Start, Adding, Delete, Update, Listing and Search methods are added to PhoneBook class.

---
+++ courses/PhoneBookProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PhoneBookProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook phoneBook = new PhoneBook();
+            phoneBook.Start();
+        }
+    }
+}
